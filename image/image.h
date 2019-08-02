1261b831cd76d36e6a02cc5cd2255952877cc7bb
Jonas Minnberg <sasq64@gmail.com>
2019-08-02 14:22:02 +0200

    Forgot inline

--- image/image.h
+++ image/image.h
@@ -19,8 +19,24 @@ private:
 
 std::vector<bitmap> load_gifs(const std::string& filename);
 bitmap load_image(const std::string& file_name);
-// bitmap load_png(const std::string &file_name);
 void save_png(bitmap bitmap, const std::string& path);
 
 } // namespace image
 
+namespace utils {
+inline bool load_data(image::bitmap& target, std::string const& file_name)
+{
+    try {
+        target = image::load_image(file_name);
+    } catch (image::image_exception& ie) {
+        return false;
+    }
+    return true;
+}
+
+inline void save_data(image::bitmap const& source, std::string const& file_name)
+{
+    image::save_png(source, file_name);
+}
+
+} // namespace utils
